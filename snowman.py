69536d01135c2a7efb73271f59a66ccd3ea80932
v3gas90b <v3gas90b@gmail.com>
2025-12-06 16:54:27 +0100

    Updated snowman.py - Added replay option after game ends

--- snowman.py
+++ snowman.py
@@ -1,4 +1,9 @@
 from game_logic import play_game
 
 if __name__ == "__main__":
-    play_game()
+    while True:
+        play_game()
+        replay = input("Do you want to play another game? (yes/no) ")
+        if replay == "yes":
+            continue
+        break
